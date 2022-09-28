b36a8953765ed2ff467302996f8d6cd131a8091b
Wegela <gwenael.giroux@gmail.com>
2022-09-28 09:24:00 +0200

    ff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { ResetCounterComponent } from './reset-counter/reset-counter.component';
 import { RandomCounterComponent } from './random-counter/random-counter.component';
+import { DirectiveCounterDirective } from './directive/directive-counter.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
     ResetCounterComponent,
-    RandomCounterComponent
+    RandomCounterComponent,
+    DirectiveCounterDirective
   ],
   imports: [
     BrowserModule,
